a7cfb28a6aeb01933a61e2d1253a762c507e0901
Opilane TTHK <opilane@tthk.ee>
2021-10-14 09:11:15 +0300

    Week7Arrays git commit -m Week7Arrays

---
+++ Week7Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace week7Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab kasutajale hommikusöögi menüüd
+            //progrramm palub kasutajal teha valikut
+            //programm kuvab kasutaja valikut konsooli
+
+
+            Console.WriteLine("");
+            string[] food = { "omlett", "pannkoogid", "keedumuna","neljaviljapuder", "singivõileib" };
+
+
+
+            Console.WriteLine("Menüüs: ");
+
+            for (int i = 0; i < food.Length; i++)
+            {
+                Console.WriteLine(food[i]);
+            }
+
+
+            Console.WriteLine("vali toit (Sisesta number 1 - 4");
+            int userchoice = Convert.ToInt32(Console.ReadLine()) - 1;
+            Console.WriteLine($"Oled valinud {food[userchoice]}");
+
+        }
+    }
+}
